ee44e8569d6994317ae5f44b1fec4518d2468361
Michele Mola <michele.mola92@gmail.com>
2020-04-21 18:55:09 +0200

    Initial Commit

---
+++ Pokedex2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex2.0
+//
+//  Created by Michele Mola on 21/04/2020.
+//  Copyright © 2020 Michele Mola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
